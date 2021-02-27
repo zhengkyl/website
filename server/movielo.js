22766fd2c68eee706838c44207e1992011370ce6
Kyle Zheng <kylezheng73@gmail.com>
2021-02-26 21:50:46 -0500

    update api endpoint

--- server/movielo.js
+++ server/movielo.js
@@ -1,6 +1,6 @@
 const GALLERY_ID = process.env.MOVIELO_GALLERY_ID
 
-const MOVIELO_ENDPOINT = `https://us-central1-movielo.cloudfunctions.net/api/movies/${GALLERY_ID}?limit=1`
+const MOVIELO_ENDPOINT = `https://us-central1-movielo.cloudfunctions.net/api/galleries/${GALLERY_ID}/movies?sortBy=date&order=desc&limit=1`
 
 export async function getLastWatched() {
   return fetch(MOVIELO_ENDPOINT)
