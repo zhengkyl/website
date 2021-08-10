1a629ee0bd8f6099076fb50cc539435c2d1aea30
Kyle Zheng <kylezheng73@gmail.com>
2021-08-09 23:56:37 -0400

    feat: rewrite subpaths to github

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   images: {
     domains: ['i.scdn.co','image.tmdb.org'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://zhengkyl.github.io/:path*',
+      },
+    ]
+  },
 }
