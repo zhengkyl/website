97e1ba3fb16e8bb17535954d2c4b6b4ce45e5c13
Kyle Zheng <kylezheng73@gmail.com>
2023-06-03 10:04:03 -0400

    working static generate mdx posts

---
+++ app/code/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import fs from "fs";
+import { bundleMDX } from "mdx-bundler";
+import { getMDXComponent } from "mdx-bundler/client";
+import path from "path";
+
+export const dynamicParams = false;
+
+const filesDir = path.join(process.cwd(), "posts");
+
+export async function generateStaticParams() {
+  const fileNames = fs.readdirSync(filesDir);
+  return fileNames.map((fileName) => ({
+    slug: fileName.replace(/\.mdx$/, ""),
+  }));
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Awaited<ReturnType<typeof generateStaticParams>>[number];
+}) {
+  const filePath = path.join(filesDir, `${params.slug}.mdx`);
+  const fileData = fs.readFileSync(filePath, "utf8");
+  const { code, frontmatter } = await bundleMDX({ source: fileData });
+  const Component = getMDXComponent(code);
+  console.log("page function run");
+
+  return <Component />;
+}
