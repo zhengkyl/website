2859f0620ecc51060b57140a0ec061666b95fb3e
Kyle Zheng <kylezheng73@gmail.com>
2023-06-20 01:15:28 -0400

    makes post loading done

--- app/makes/page.tsx
+++ app/makes/page.tsx
@@ -1,3 +1,55 @@
+import path from "path";
+import fs from "fs";
+import graymatter from "gray-matter";
+import { generateStaticParams } from "./[slug]/page";
+import Link from "next/link";
+
+export const makesDir = path.join(process.cwd(), "posts/makes");
+
+type Frontmatter = {
+  slug: string;
+  title: string;
+  summary: string;
+  posted: string;
+  edited: string;
+};
+
+export function getSlugs() {
+  const filesDir = path.join(process.cwd(), "posts/makes");
+  const fileNames = fs.readdirSync(filesDir);
+  return fileNames.map((fileName) => {
+    const [_, slug] = fileName.match(/(.+)\.mdx$/);
+    return { slug };
+  });
+}
+
 export default async function Page() {
-  return <div>nothing here yet</div>;
+  const frontmatters = getSlugs().map(({ slug }) => {
+    const filePath = path.join(makesDir, `${slug}.mdx`);
+    const fileData = fs.readFileSync(filePath, "utf8");
+    const file = graymatter(fileData);
+    return { slug, ...file.data } as Frontmatter;
+  });
+
+  // sort reverse chronological
+  frontmatters.sort((a, b) => Date.parse(b.posted) - Date.parse(a.posted));
+
+  return (
+    <div>
+      <h1>/makes</h1>
+      <ul>
+        {frontmatters.map((matter) => (
+          <Link href={`/makes/${matter.slug}`}>
+            <li className="my-4 hover:underline">
+              <div className="flex justify-between items-center">
+                <h2 className="">{matter.title}</h2>
+                <small className="">{matter.posted}</small>
+              </div>
+              <p className="text-2xl">{matter.summary}</p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 }
