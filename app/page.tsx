299166072fce1372abd397e0e9ffe8e31e710891
Kyle Zheng <kylezheng73@gmail.com>
2023-06-19 21:16:26 -0400

    start verbifying

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
 import { getLatestReviewAndMovie } from "../lib/server/movielo";
 import Home from "./Home";
 
 export default async function HomePage() {
-  const movieData = await getLatestReviewAndMovie();
-
-  return <Home movieData={movieData} />;
+  // const movieData = await getLatestReviewAndMovie();
+  // return <Home movieData={movieData} />;
+  return (
+    <div>
+      <h1>Kyle Zheng</h1>
+      <Link href="/makes">
+        <h2>/makes</h2>
+      </Link>
+      <Link href="/likes">
+        <h2>/likes</h2>
+      </Link>
+    </div>
+  );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,37 +3,35 @@ import { Container } from "./basic";
 
 export default function Footer() {
   return (
-    <Container className="py-8 mt-16">
-      <footer className="flex justify-between flex-wrap items-center">
-        <div className="flex gap-4">
-          <a
-            href="https://github.com/zhengkyl"
-            className="hover:underline font-bold text-rose-200 highlight p-3"
-          >
-            github
-          </a>
-          <a
-            href="https://www.linkedin.com/in/zhengkyl/"
-            className="hover:underline font-bold text-rose-200 highlight p-3"
-          >
-            linkedin
-          </a>
-        </div>
-        <p className="flex-grow text-right">
-          made with{" "}
-          <Link href="./images/broccoli.jpg" target="_blank">
-            🥦
-          </Link>{" "}
-          •{" "}
-          <Link href="./resume.pdf" target="_blank">
-            hire me
-          </Link>{" "}
-          •{" "}
-          <a href="https://github.com/zhengkyl/website" target="_blank">
-            source
-          </a>
-        </p>
-      </footer>
-    </Container>
+    <footer className="flex justify-between flex-wrap items-center">
+      <div className="flex gap-4">
+        <a
+          href="https://github.com/zhengkyl"
+          className="hover:underline font-bold text-rose-200 highlight p-3"
+        >
+          github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/zhengkyl/"
+          className="hover:underline font-bold text-rose-200 highlight p-3"
+        >
+          linkedin
+        </a>
+      </div>
+      <p className="flex-grow text-right">
+        made with{" "}
+        <Link href="./images/broccoli.jpg" target="_blank">
+          🥦
+        </Link>{" "}
+        •{" "}
+        <Link href="./resume.pdf" target="_blank">
+          hire me
+        </Link>{" "}
+        •{" "}
+        <a href="https://github.com/zhengkyl/website" target="_blank">
+          source
+        </a>
+      </p>
+    </footer>
   );
 }
