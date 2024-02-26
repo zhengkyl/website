88509495b53581221170ec85bd52861553e39650
Kyle Zheng <kylezheng73@gmail.com>
2024-02-26 04:14:05 -0500

    update deps

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css"
-        />
-      </head>
+      <head></head>
       <body>{children}</body>
     </html>
   );
