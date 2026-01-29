a6ce8b4cbdea9d10ad7f162386a2a24cdd6f40dd
Kyle Zheng <kylezheng73@gmail.com>
2026-01-29 04:39:49 -0500

    projects + redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,10 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), UnoCSS(), preact()],
+  redirects: {
+    "/posts/ssh_game_of_life": "/writes/posts/ssh-game-of-life",
+    "/posts/crafting_qr_codes": "/writes/posts/crafting-qr-codes",
+    "/posts/few_word_why_tailwind_not_bad": "/writes/notes/few-word-why-tailwind-good",
+    "/posts/time_travel_homework_2026": "/writes/notes/time-travel-homework-2026",
+  }
 });
